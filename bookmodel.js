f0ba16501482cc3a084ef514e2694db0706f2705
Shania Kimbrough <shania_nicolebb@yahoo.com>
2021-10-23 19:29:36 -0400

    added seeds

---
+++ bookmodel.js
@@ -0,0 +1,11 @@
+'use strict'
+const mongoose = require('mongoose');
+const Book = require('./schema');
+
+
+const BookModel = mongoose.model('bookCan', Book);
+
+
+
+
+module.exports = BookModel;
---
+++ seed.js
@@ -0,0 +1,36 @@
+'use strict';
+
+const BookModel = require("./bookmodel");
+
+function seed (req, res){
+    const seedArr = [
+        {
+            title: 'The Hobbit',
+            description: 'A book about some friends',
+            status: 'completed',
+            email: 'frodo@yahoo.com'
+
+        },
+        {
+            title: 'Carrie',
+            description: 'A book about a bullied girl',
+            status: 'deceased',
+            email: 'pigblood@yahoo.com'
+
+        },
+        {
+            title: 'A hungry caterpillar',
+            description: 'A book about a very hungry caterpillar',
+            status: 'hungry',
+            email: 'caterpillar@yahoo.com'
+
+        }
+    ]
+    seedArr.forEach((seed) => {
+        let entry = new BookModel(seed)
+        // entry.save();
+    })
+    res.status(200).send('Database seeded'); 
+
+}
+module.exports = seed;

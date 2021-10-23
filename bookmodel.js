784be22c023c411f71b765d921c45bc605efc9ee
Ian Cargill <idcargill@gmail.com>
2021-10-23 16:56:30 -0700

    Ian: Add Book Route- PRETTIER

--- bookmodel.js
+++ bookmodel.js
@@ -1,11 +1,7 @@
-'use strict'
-const mongoose = require('mongoose');
-const Book = require('./schema');
+"use strict";
+const mongoose = require("mongoose");
+const Book = require("./schema");
 
-
-const BookModel = mongoose.model('bookCan', Book);
-
-
-
+const BookModel = mongoose.model("bookCan", Book);
 
 module.exports = BookModel;
---
+++ getBooks.js
@@ -0,0 +1,10 @@
+const BookModel = require("./bookmodel");
+
+async function getBooks(req, res) {
+  await BookModel.find((err, books) => {
+    if (err) return res.status(500).send("No BOOKS FOR YOU!");
+    res.status(200).send(books);
+  });
+}
+
+module.exports = getBooks;

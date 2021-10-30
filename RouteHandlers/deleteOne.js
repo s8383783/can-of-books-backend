5a8770751cae14ca4b9de01b42849f3c7b756fbe
brandonmizutani <bran2miz@gmail.com>
2021-10-30 11:36:02 -0700

    fixed delete code

--- RouteHandlers/deleteOne.js
+++ RouteHandlers/deleteOne.js
@@ -2,29 +2,12 @@ const BookModel = require("../Schema/bookmodel");
 
 async function deleteOne(req, res) {
   try {
-    console.log("deleteOne Action");
-    const bookId = req.params.id;
-    const email = req.query.email;
-
-    const bookToDelete = await BookModel.findOne({_id: bookId, email});
-
-    if(!bookToDelete) {
-      res.status(400).send('Error: Book could not be deleted.');
-      return;
-    }
-    if (bookToDelete.email !== email) {
-      res.status(400).send('Error: Book could not be deleted.');
-      return;
-    }
-    
-    await BookModel.findByIdAndDelete(id);
-    res.send('success');
-
-  }
-  catch (error) {
-    console.error(error);
-    res.status(400).send('Error: Book could not be deleted.');
-  }
+    await BookModel.findByIdAndDelete(req.params.id);
+    res.status(204).send('success')
+  } catch (error) {
+    console.error.apply(error);
+    res.status(500).send('server error')
+  } 
 }
 
 module.exports = deleteOne;
